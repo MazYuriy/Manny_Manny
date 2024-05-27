3759ca4cbc92cd76cf723ca6092a1db817023953
MazYuriy <gerasimcug@gmail.com>
2024-05-27 16:49:12 +0300

    to pizdezz

--- Unity.Game.Project.Student/Assets/Scripts/Player/MainCamera.cs
+++ Unity.Game.Project.Student/Assets/Scripts/Player/MainCamera.cs
@@ -6,8 +6,8 @@ public class MainCamera : MonoBehaviour
 {
     public float sensitivity = 100f;
     public Transform Player;
-    float xRot = 0f;
-    float yRot = 0f;
+    float xRot ;
+    float yRot ;
     void Start()
     {
 
@@ -18,8 +18,8 @@ public class MainCamera : MonoBehaviour
         float mousX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mousY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
-        xRot -= mousY;  
-        xRot = Mathf.Clamp(xRot, -90f, 90f);
+        yRot -= mousY;  
+        yRot = Mathf.Clamp(yRot, -90f, 90f);
 
         Player.Rotate(Vector3.up * mousX);
 
